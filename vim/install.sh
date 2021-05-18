e8eee663df544abcbab7294a517625e034d4e75a
answerywj <1045837697@qq.com>
2021-05-18 20:07:25 +0800

    update vim install script

--- vim/install.sh
+++ vim/install.sh
@@ -5,13 +5,12 @@
 
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
-	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
+    echo "You are using apt-get tool"
+	sudo apt-get install -y -f vim ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
-	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
-fi
-
-##Add HomeBrew support on  Mac OS
-if which brew >/dev/null;then
+    echo "You are using yum tool"
+	sudo yum install -y -f gcc vim git ctags xclip astyle python-setuptools python-devel
+elif which brew >/dev/null;then
     echo "You are using HomeBrew tool"
     brew install vim ctags git astyle
 fi
@@ -19,7 +18,7 @@ fi
 sudo easy_install -ZU autopep8 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 mv -f ~/vim ~/vim_old
-cd ~/ && git clone https://github.com/OhOhOhOhOhOhOhOh/vim.git
+git clone https://github.com/OhOhOhOhOhOhOhOh/vim.git ~/vim
 
 ## 备份
 mv -f ~/.vim ~/.vim_old
